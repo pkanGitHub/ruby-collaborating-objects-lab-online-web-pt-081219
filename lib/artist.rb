841759e16cc59952e3fbeab9dad153dc057ae69b
Phyllis Kan <pyk9q7@gmail.com>
2019-09-30 22:45:05 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,7 +20,10 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-     self.find(name) ? self.find(name) : self.new(name)
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
   end
 
   def self.find(name)
